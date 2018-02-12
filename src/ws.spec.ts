607f0d1c480f82786bc3a3610892aa6b4b5e06f7
Homa Wong <homawong@gmail.com>
2018-02-12 00:51:10 -0800

    init commit

---
+++ src/ws.spec.ts
@@ -0,0 +1,135 @@
+import { AssertOrder } from 'assertron'
+import { spec, registerPlugin } from 'komondor'
+import WebSocket = require('ws')
+
+import * as wsPlugin from './index'
+
+registerPlugin(wsPlugin)
+
+test('ws verify', async () => {
+  const wsSpec = await spec(WebSocket)
+  const ws = new wsSpec.subject('ws://html5rocks.websocket.org/echo')
+
+  const actionCount = new AssertOrder()
+  wsSpec.onAny(() => { actionCount.exactly(1, 6) })
+
+  ws.on('open', () => { ws.send('Ping') })
+
+  const order = new AssertOrder(2)
+  ws.on('message', (data) => {
+    expect(data).toBe('Ping')
+    order.once(1)
+    ws.terminate()
+  })
+
+  ws.on('close', () => { order.once(2) })
+  await order.wait(2)
+
+  await wsSpec.satisfy([
+    { type: 'ws/constructor', payload: ['ws://html5rocks.websocket.org/echo'] },
+    { type: 'ws/event', meta: { event: 'open' } },
+    { type: 'ws/send', payload: 'Ping' },
+    { type: 'ws/message', payload: 'Ping' },
+    { type: 'ws/terminate' },
+    { type: 'ws/event', meta: { event: 'close' } }
+  ])
+  order.end()
+  actionCount.end()
+})
+
+test('ws save', async () => {
+  const wsSpec = await spec.save('ws/echo/success', WebSocket)
+  const ws = new wsSpec.subject('ws://html5rocks.websocket.org/echo')
+
+
+  const actionCount = new AssertOrder()
+  wsSpec.onAny(() => { actionCount.exactly(1, 6) })
+
+  ws.on('open', () => { ws.send('Ping') })
+
+  const order = new AssertOrder(2)
+  ws.on('message', (data) => {
+    expect(data).toBe('Ping')
+    order.once(1)
+    ws.terminate()
+  })
+
+  ws.on('close', () => { order.once(2) })
+  await order.wait(2)
+
+  await wsSpec.satisfy([
+    { type: 'ws/constructor', payload: ['ws://html5rocks.websocket.org/echo'] },
+    { type: 'ws/event', meta: { event: 'open' } },
+    { type: 'ws/send', payload: 'Ping' },
+    { type: 'ws/message', payload: 'Ping' },
+    { type: 'ws/terminate' },
+    { type: 'ws/event', meta: { event: 'close' } }
+  ])
+  order.end()
+  actionCount.end()
+})
+
+
+test.skip('ws simulate', async () => {
+  const wsSpec = await spec.simulate('ws/echo/success', WebSocket)
+  const ws = new wsSpec.subject('ws://html5rocks.websocket.org/echo')
+
+  const actionCount = new AssertOrder()
+  wsSpec.onAny(() => { actionCount.exactly(1, 6) })
+
+  ws.on('open', () => { ws.send('Ping') })
+
+  const order = new AssertOrder(2)
+  ws.on('message', (data) => {
+    expect(data).toBe('Ping')
+    order.once(1)
+    ws.terminate()
+  })
+
+  ws.on('close', () => { order.once(2) })
+  await order.wait(2)
+
+  await wsSpec.satisfy([
+    { type: 'ws/constructor', payload: ['ws://html5rocks.websocket.org/echo'] },
+    { type: 'ws/event', meta: { event: 'open' } },
+    { type: 'ws/send', payload: 'Ping' },
+    { type: 'ws/message', payload: 'Ping' },
+    { type: 'ws/terminate' },
+    { type: 'ws/event', meta: { event: 'close' } }
+  ])
+  order.end()
+  actionCount.end()
+})
+
+
+test('ws simulate on not exist record will spy', async () => {
+  const wsSpec = await spec.simulate('ws/echo/notExist', WebSocket)
+  const ws = new wsSpec.subject('ws://html5rocks.websocket.org/echo')
+
+
+  const actionCount = new AssertOrder()
+  wsSpec.onAny(() => { actionCount.exactly(1, 6) })
+
+  ws.on('open', () => { ws.send('Ping') })
+
+  const order = new AssertOrder(2)
+  ws.on('message', (data) => {
+    expect(data).toBe('Ping')
+    order.once(1)
+    ws.terminate()
+  })
+
+  ws.on('close', () => { order.once(2) })
+  await order.wait(2)
+
+  await wsSpec.satisfy([
+    { type: 'ws/constructor', payload: ['ws://html5rocks.websocket.org/echo'] },
+    { type: 'ws/event', meta: { event: 'open' } },
+    { type: 'ws/send', payload: 'Ping' },
+    { type: 'ws/message', payload: 'Ping' },
+    { type: 'ws/terminate' },
+    { type: 'ws/event', meta: { event: 'close' } }
+  ])
+  order.end()
+  actionCount.end()
+})
